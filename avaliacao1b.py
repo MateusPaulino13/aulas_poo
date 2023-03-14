ce6a25cff3ca62e3d3059bcc688948eb9387891c
Mateus Paulino <80184052+MateusPaulino13@users.noreply.github.com>
2023-03-14 11:38:25 -0300

    Adicionando Avaliação e desafio

---
+++ avaliacao1b.py
@@ -0,0 +1,59 @@
+# Primeiro desafio
+
+# age = int(input("Digite sua idade: "))
+
+# if(age >= 18):
+#     print("Você é maior de idade")
+# else: 
+#     print("Você é menor de idade")
+
+
+
+# Segundo desafio
+
+# num = int(input("Digite um número: "))
+
+# if(num > 0):
+#     print("O número é positivo")
+# elif(num == 0):
+#     print("O número é igual a zero")
+# else:
+#     print("O número é negativo")
+
+
+# Terceiro Desafio
+
+# age = int(input("Digite sua idade: "))
+# height = float(input("Digite sua altura em Metros: "))
+
+# if(age >= 18 and age <= 35 and height > 1.60):
+#     print("Você pode participar do concurso")
+# else:
+#     print("Você não pode participar")
+
+
+
+# Quarto desadio
+
+# peso = float(input("Digite seu peso em Kg: "))
+# altura = float(input("Digite sua algura em Metros: "))
+# imc = round(peso / (pow(altura, 2)), 2)
+
+# if(imc < 18.5):
+#     print("Seu peso está baixo")
+# elif(imc >= 18.8 and imc < 24.9):
+#     print("Seu peso está normal")
+# elif(imc >= 25 and imc < 29.9):
+#     print("Você está acima do peso")
+# else:
+#     print("Você está com obesidade")
+
+
+# Quinto desafio
+
+num = int(input("Digite um número: "))
+
+if(num % 2 == 0):
+    print("O número é par")
+else:
+    print("O número é impar")
---
+++ desafio.py
@@ -0,0 +1,9 @@
+# Desafio para a proxima aula
+
+import time as t
+
+print("Bem-Vindo à corrida de obstáculos!")
+
+t.sleep(1)
+
+resposta = input("Qual é a altura da proxima (em Metros)? ")
