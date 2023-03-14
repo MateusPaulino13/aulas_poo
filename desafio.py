4a11abd5788b82531db36fc3d8f08dd9d1b2a977
Mateus Paulino <80184052+MateusPaulino13@users.noreply.github.com>
2023-03-14 12:12:09 -0300

    Desenvolvimento do Desafio

--- desafio.py
+++ desafio.py
@@ -1,9 +1,27 @@
 # Desafio para a proxima aula
-
 import time as t
+
+contador = 0
 
 print("Bem-Vindo à corrida de obstáculos!")
 
 t.sleep(1)
 
-resposta = input("Qual é a altura da proxima (em Metros)? ")
+resposta = float(input("Qual é a altura da proxima (em Metros)? "))
+
+while(resposta <= 1.5):
+    contador += 1
+    t.sleep(1)
+
+    print("Parabéns! Você pulou a barreira com sucesso")
+    print("")
+
+    resposta = float(input("Qual é a altura da proxima (em Metros)? "))
+    
+    if(resposta > 1.5):
+        print("Ops, Você não conseguiu pular da barreira! ")
+
+        resposta = float(input("Qual é a altura da proxima (em Metros)? "))
+    if(type(resposta) != float):
+        print(f"Você pulou com sucesso {contador} barreiras")
+        break
