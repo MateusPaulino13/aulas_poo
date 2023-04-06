35338305d8423bf791feb0244bf840a62bb3e046
Mateus Paulino <80184052+MateusPaulino13@users.noreply.github.com>
2023-04-06 08:57:13 -0300

    Adicionando Primeira Aula

---
+++ aulaPoo1.py
@@ -0,0 +1,17 @@
+# variaveis
+# a = "Mateus Paulino"
+# b = 5.8
+# c = str(b) # valor retornado: '5.8'
+
+# # para descobrir qual o tipo da variavel, basta apenas usar o a função type()
+# print(type(a))
+
+# # lista
+# Users = ["Mateus Paulino", "Ketlyn Dantas", "Nycollas José"]
+
+num1 = int(input("Digite o primeiro número: "))
+num2 = int(input("Digite o segundo número: "))
+
+soma = num1 + num2
+
+print(f"A soma dos dois números foi: {soma}")
