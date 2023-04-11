9a4b384744fc0534d48011e687bb21052599c9ab
Mateus Paulino <80184052+MateusPaulino13@users.noreply.github.com>
2023-04-11 14:15:58 -0300

    Adicionando Aula de Loops

--- aula7.py
+++ aula7.py
@@ -1,6 +1,27 @@
 # Laços de repetição
+# While
 i = 0
 
 while i <= 10:
     print(f"Valor: {i}")
     i += 1
+
+# for
+names = ["Mateus", "Nycollas", "Ketlyn", "Ana", "Luiza", "Felipe"]
+
+for i in names:
+    print(i)
+
+# exemplos
+# for i in range(10):
+#     print(i)
+
+
+# for i in names:
+#     if(i == "Ketlyn"):
+#         continue
+    
+#     print(i)
+
+for i in range(2, 19, 2):
+    print(i)
