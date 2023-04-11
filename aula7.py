a40ca64cf75f2b28cd875891895723eb49a691f2
Mateus Paulino <80184052+MateusPaulino13@users.noreply.github.com>
2023-04-11 14:18:01 -0300

    Colocando Exercícios

--- aula7.py
+++ aula7.py
@@ -23,5 +23,22 @@ for i in names:
     
 #     print(i)
 
-for i in range(2, 19, 2):
-    print(i)
+# for i in range(2, 19, 2):
+#     print(i)
+
+
+# num = int(input("Digite um número para a tabuada: "))
+# mult = int(input("Digite um número para multiplicar: "))
+
+# for i in range(1, mult + 1):
+#   result = num * i
+#   print(f"{num} X {i} = {result}")
+
+
+for i in range(1, 6, 1):
+  num = int(input("Digite sua nota: "))
+  soma += num
+
+
+
+print(soma / 5)
