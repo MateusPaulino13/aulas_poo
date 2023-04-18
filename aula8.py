73c32ff06ec7b1b5f80f5d4d7c371eb57bba2124
Mateus Paulino <80184052+MateusPaulino13@users.noreply.github.com>
2023-04-18 11:46:19 -0300

    Aula de Loops

---
+++ aula8.py
@@ -0,0 +1,14 @@
+# soma = 0
+
+# for i in range(5):
+#     num = float(input("Digite a nota: "))
+#     soma += num
+
+# media = soma / 5
+# print(f"Sua média é de: {media}")
+
+
+for i in range(11):
+    for a in range(11):
+        print(f"{i} X {a} = {i*a}")
+        print("|------------|")
