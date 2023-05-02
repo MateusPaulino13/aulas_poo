68b40e32a949e8103c3b9c624abf17263f57d8a4
Mateus Paulino <80184052+MateusPaulino13@users.noreply.github.com>
2023-05-02 12:21:23 -0300

    Criação exercício de Funções

---
+++ exercicio_func.py
@@ -0,0 +1,30 @@
+# função de soma
+
+# def soma(a, b):
+#     return a + b
+
+# print(soma(5, 6))
+
+# Função Fatorial
+# numero = int(input("Digite um Número: ") )
+
+# def fatorial(numero):
+#     resultado = 1
+#     limite = 15
+
+#     while limite <= numero:
+#         resultado *= limite
+#         limite += 1
+
+#     return resultado
+
+# print(fatorial(numero))
+
+
+# Média aritmédica
+numeros = [10, 7, 4, 9, 6]
+
+def media(num):
+    return sum(num) / len(num)
+
+print(media(numeros))
