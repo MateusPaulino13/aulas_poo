be221ff6da03c8922d32de083941b18d7192a622
Mateus Paulino <80184052+MateusPaulino13@users.noreply.github.com>
2023-05-09 12:03:25 -0300

    Adicionando exercicíos prática

---
+++ exercicios_pratico.py
@@ -0,0 +1,32 @@
+# //--Saudação--//
+
+# def saudacao():
+# name = input("Digite seu nome: ")
+# age = int(input("Digite sua idade: "))
+#     return (f"Olá {name}, seja bem-vindo. Você tem {age} anos")   
+
+# print(saudacao()) 
+
+
+# Soma
+
+# def soma():
+# num1 = int(input("Digite o primeiro número: "))
+# num2 = int(input("Digite o segundo número: "))
+#     return (f"A soma dos número é igual a: {n1 + n2}")
+
+
+# print(soma())
+
+
+# Média
+
+def media():
+    n1 = float(input("Digite o primeira nota: "))
+    n2 = float(input("Digite o segunda nota: "))
+    n3 = float(input("Digite o terceira nota: "))
+
+    return (f"Sua média final é de: {(n1 + n2 + n3) / 3}")
+
+
+print(media())
