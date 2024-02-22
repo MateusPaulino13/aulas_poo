55f5be19035d1c67f52cf0e3a40f123a11d63a9c
Mateus Paulino <mateusgpaulino@gmail.com>
2024-02-22 19:46:04 -0300

    modificando exercicio
    verificando se o senior possui filhos para ter um segundo aumento de 500 reais

--- Sistemas de Informação/1Semestre/aumentoSalarial.py
+++ Sistemas de Informação/1Semestre/aumentoSalarial.py
@@ -2,11 +2,18 @@ salario = float(input("Digite seu salário atual: "))
 nivel = input("Digite o seu nível conhecimento (Júnior, Pleno ou Sênior): ")
 
 if(nivel == "Junior" or nivel == "junior" or nivel == "Júnior" or nivel == "júnior"):
-    aumento = salario * 1.1
-    print(f"Seu salário com aumento é de R${salario} e você é júnior")
+    salario = salario * 1.1
+    print(f"Seu salário com aumento é de R${salario:.2f} e você é júnior")
 elif(nivel == "Pleno" or nivel == "pleno"):
-    aumento = salario * 1.2
-    print(f"Seu salário com aumento é de R${salario} e você é Pleno")
+    salario = salario * 1.2
+    print(f"Seu salário com aumento é de R${salario:.2f} e você é Pleno")
 elif(nivel == "Sênior" or nivel == "sênior" or nivel == "Senior" or nivel == "senior"):
-    aumento = salario * 1.3
-    print(f"Seu salário com aumento é de R${salario} e você é Sênior")
+    temFilho = input("Você possui filhos? Digite sim ou não: ")
+    if(temFilho == "Sim" or temFilho == "sim"):
+        salario = (salario * 1.3) + 500
+        print(f"Seu salário com aumento é de R${salario:.2f} e você é Sênior")
+    else:
+        salario = salario * 1.3
+        print(f"Seu salário com aumento é de R${salario:.2f} e você é Sênior")
+else:
+    print("Cargo desconhecido!")
